1389da28a0f11cd1f7ace90c11e7dad1ce47b7a7
Megan Zehrt <zehrtmegan@gmail.com>
2024-10-04 20:32:29 -0500

    first commit

---
+++ algo.js
@@ -0,0 +1,42 @@
+// 287. Find The Duplicate Number
+
+
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and using only constant extra space.
+
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    
+    let slow = 0
+    let fast = 0
+
+    while(true){
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+
+        if(fast == slow){
+            break
+        }
+    }
+
+    let slow2 = 0
+    while(true){
+        slow = nums[slow]
+        slow2 = nums[slow2]
+
+        if(slow == slow2){
+            return slow
+        }
+    }
+};
